651022a74db3ef1ddba376c08cb5895b198348fd
Huynh Hoang Huy <akirahuynh2002@gmail.com>
2022-07-18 15:31:02 +0700

    UPDATE - Some minor environmental changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ['node_modules/flowbite/**/*.js', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        padding: '10rem',
+      },
+      keyframes: {
+        'vertically-moving': {
+          '0%': {
+            transform: 'translateY(-20px)',
+          },
+          '100%': {
+            transform: 'translateY(0px)',
+          },
+        },
+      },
+      animation: {
+        'vertically-moving': 'vertically-moving 2s linear infinite alternate',
+      },
+    },
   },
   plugins: [require('flowbite/plugin')],
 }
