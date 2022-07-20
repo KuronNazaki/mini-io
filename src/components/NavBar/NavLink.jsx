176d68b35c36e07b833b95144fd31683aa02a38f
Huynh Hoang Huy <akirahuynh2002@gmail.com>
2022-07-21 00:04:06 +0700

    FIX - Fix display error of the nav bar item

--- src/components/NavBar/NavLink.jsx
+++ src/components/NavBar/NavLink.jsx
@@ -7,13 +7,9 @@ const STATUS_STYLES = {
     'text-lg block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-teal-500 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700',
 }
 
-const NavLink = ({ name, path, children, onChoose, active }) => {
+const NavLink = ({ path, children, active }) => {
   return (
-    <Link
-      to={path}
-      className={STATUS_STYLES[active.toString()]}
-      onClick={() => onChoose(name)}
-    >
+    <Link to={path} className={STATUS_STYLES[active.toString()]}>
       {children}
     </Link>
   )
@@ -23,7 +19,6 @@ NavLink.propTypes = {
   name: PropTypes.string.isRequired,
   path: PropTypes.string.isRequired,
   children: PropTypes.node,
-  onChoose: PropTypes.func.isRequired,
   active: PropTypes.bool,
 }
 
